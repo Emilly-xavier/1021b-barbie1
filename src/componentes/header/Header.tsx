caa8e54e43b39de0ff1c28718de0e9c9232c62a6
Emilly-xavier <emilly.xavier@estudante.ifms.edu.br>
2023-09-28 10:01:02 -0400

    Atualizando commit...

--- src/componentes/header/Header.tsx
+++ src/componentes/header/Header.tsx
@@ -1,27 +1,28 @@
 import './Header.css'
+import { Link } from 'react-router-dom'
 function Header(){
    
     return(
         <>
         <header>
             <div>
-                <h1>Barbie</h1>
+                <Link to="/">Barbie</Link>
             </div>
             <nav>
                 <ul>
 
 
                     <li>
-                        <a href="#">Página inicial</a>
+                        <Link to="/">Página inicial</Link>
                     </li>
                     <li>
-                        <a href="#">Sobre</a>
+                        <Link to="/sobre">Sobre</Link>
                     </li>
                     <li>
-                        <a href="#">Notícias</a>
+                        <Link to="/noticias">Notícias</Link>
                     </li>
                     <li>
-                        <a href="#">Contato</a>
+                        <Link to="/contato">Contato</Link>
                     </li>
                 </ul>
             </nav>
